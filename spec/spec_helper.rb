02ae536be1b4df4120b01d00f2496ba2eb2249b9
Carl Mercier <carl@carlmercier.com>
2017-05-31 21:57:23 -0400

    Allow focus tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require "temping"
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
+  config.filter_run_when_matching :focus
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
